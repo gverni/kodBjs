98e107a9df1817eb3ef67c91c0f02650bd538291
Giuseppe Verni <gverni@frn-ub12-19.qualcomm.com>
2016-03-08 18:27:14 +0000

    Added vendor folder

---
+++ nw/install_vendor.sh
@@ -0,0 +1,5 @@
+cp -r node_modules/backbone/ src/app/vendor/
+cp -r node_modules/underscore/ src/app/vendor/
+cp -r node_modules/jquery/ src/app/vendor/
+cp -r node_modules/bootstrap/ src/app/vendor/
+
